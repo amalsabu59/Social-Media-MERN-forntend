d2a6292c0cb9bdac7ec42920a1eb460d7fd242db
amalsabu59 <amalsabu59@gmail.com>
2022-02-21 16:45:15 +0530

    Navbar updated

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,46 @@
+import "./topbar.css"
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+function Topbar() {
+  return (
+    <div className="topBarContainer">
+        <div className="topbarLeft">
+            <span className="logo">HelloClub</span>
+        </div>
+        <div className="topbarCenter">
+            <div className="searchBar">
+                    <SearchIcon className="searchIcon"/>
+                <input placeholder="search" className="searchInput" />
+            </div>
+        </div>
+        <div className="topbarRight">
+            <div className="topbarLinks">
+                <span className="topbarLinks">Homepage</span>
+                <span className="topbarLinks">timeline</span>
+            </div>
+            <div className="topbarIcons">
+                <div className="topbarIconItem">
+                    <PersonIcon/>
+                <span className="topbarIconBadge">1</span>
+
+                </div>
+                <div className="topbarIconItem">
+                    <ForumIcon/>
+                <span className="topbarIconBadge">2</span>
+
+                </div>
+                <div className="topbarIconItem">
+                    <NotificationsIcon/>
+                <span className="topbarIconBadge">1</span>
+
+                </div>
+            </div>
+            <img src="/assets/person/1.jpg" alt="" className="topbarImage"/>
+        </div>
+    </div>
+  )
+}
+
+export default Topbar
